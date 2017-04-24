0756ce798a197b2c420d7e458f308ab963cbb8eb
Marc Hulcelle <mh@bluefrogrobotics.com>
2017-04-24 15:22:01 +0200

    [LOCAL] fixed local broadcast with new Unity Version

--- MobileApp/Assets/Scripts/LocalConnection/BroadcastServer.cs
+++ MobileApp/Assets/Scripts/LocalConnection/BroadcastServer.cs
@@ -2,14 +2,14 @@
 using UnityEngine.Networking;
 using System.Collections;
 
-public class BroadcastServer : NetworkDiscovery
+public class BroadcastServer : UnityEngine.Networking.NetworkDiscovery
 {	
 	void Start()
 	{
-//		broadcastPort = 47777;
-//		broadcastKey = 1000;
-//		broadcastVersion = 1;
-//		broadcastSubVersion = 1;
-//		broadcastData = "HELLO";
-	}
+        broadcastPort = 47777;
+        broadcastKey = 1000;
+        broadcastVersion = 1;
+        broadcastSubVersion = 1;
+        broadcastData = "HELLO";
+    }
 }
