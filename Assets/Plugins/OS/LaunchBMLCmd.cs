2151c17c01aba524b3ce881d657d238b7ca85c89
Marc Hulcelle <mh@bluefrogrobotics.com>
2018-04-16 15:02:11 +0200

    [BML] Add 'launchBML' command

---
+++ Assets/Plugins/OS/LaunchBMLCmd.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace Buddy.Command
+{
+    /// <summary>
+    /// When executed : launches a BML with the given name.
+    /// </summary>
+    public class LaunchBMLCmd : ACommand
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public LaunchBMLCmd()
+        {
+
+        }
+
+        /// <summary>
+        /// When executed : launches a BML with the given name.
+        /// </summary>
+        /// <param name="iName">Name of the BML.</param>
+        public LaunchBMLCmd(string iName)
+        {
+            Parameters = new CommandParam();
+            Parameters.Strings = new string[] { iName };
+        }
+
+        /// <summary>
+        /// Execute the command behaviour.
+        /// </summary>
+        protected override void ExecuteImpl()
+        {
+
+        }
+    }
+}
