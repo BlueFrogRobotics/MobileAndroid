a5dc94dac2164f8c984f630d34ca3424d97480a3
walid <wa@bluefrogrobotics.com>
2018-05-03 15:14:06 +0200

    [APP] added firebase notifications integration

---
+++ Assets/Scripts/Firebase/FirebaseConnection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirebaseConnection : MonoBehaviour {
+
+    //private AndroidJavaObject currentActivity;
+
+    public void Start()
+    {
+        Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
+        Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
+        //AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        //currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+        //currentActivity.Call("getToken");
+    }
+
+    public void OnTokenReceived(object sender, Firebase.Messaging.TokenReceivedEventArgs token)
+    {
+        Debug.Log("avant token");
+        Debug.Log("Received Registration Token: " + token.Token);
+    }
+
+    public void OnMessageReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs e)
+    {
+        Debug.Log("Received a new message from: " + e.Message.From);
+        Debug.Log("Raw data message: " + e.Message.RawData);
+    }
+}
